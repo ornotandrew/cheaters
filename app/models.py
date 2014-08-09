f27ff399591c58967ea63efd5afa6fb85a32271d
BLLBRE006 <brendan1b@hotmail.com>
2014-08-09 19:17:01 +0200

    added submission model to models.py and edited gitignore

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Submission(models.Model):
+    submission_id = models.IntegerField
+    user_id = models.CharField
+    course_id = models.CharField
+    assignment_number = models.IntegerField
+    file = models.CharField
+    fingerprint = models.CharField
+    date = models.DateTimeField('date submitted')
