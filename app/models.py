6999567aa0df65e3a71e880dda338106e4d671ca
BLLBRE006 <brendan1b@hotmail.com>
2014-08-09 19:35:43 +0200

    fixed submission model in models.py

--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Submission(models.Model):
-    submission_id = models.IntegerField
-    user_id = models.CharField
-    course_id = models.CharField
-    assignment_number = models.IntegerField
-    file = models.CharField
-    fingerprint = models.CharField
+    submission_id = models.IntegerField()
+    user_id = models.CharField(max_length=20)
+    course_id = models.CharField(max_length=10)
+    assignment_number = models.IntegerField()
+    file = models.TextField()
+    fingerprint = models.TextField()
     date = models.DateTimeField('date submitted')
