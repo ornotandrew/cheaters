4a4a3a03d0899011ff796f59a6e09f2efeff2c26
Andrew van Rooyen <wraith.andrew@gmail.com>
2014-08-11 09:43:28 +0200

    file line info now carries through the hashing process properly

--- temp/comparator.py
+++ temp/comparator.py
@@ -2,24 +2,25 @@ def compare():
     a = open("a.py", "r")
     b = open("b.py", "r")
     source_a = a.read()
-    print(len(source_a))
     source_b = b.read()
-    hashes_a = [get_hash_value(x[0]) for x in get_ngrams(source_a)]
-    print(len(hashes_a)*10)
-    hashes_b = [get_hash_value(x[0]) for x in get_ngrams(source_b)]
     a.close()
     b.close()
-    hash_intersection = len(set(hashes_a) & set(hashes_b))
+    ngrams_a = get_ngrams(source_a)
+    ngrams_b = get_ngrams(source_b)
+    hashes_a = get_hash_values(ngrams_a)
+    hashes_b = get_hash_values(ngrams_b)
+
+    hash_intersection = len(set([x[0] for x in hashes_a]) & set([x[0] for x in hashes_b]))
     fingerprint_a = winnow(hashes_a)
     fingerprint_b = winnow(hashes_b)
 
     # the percentages below are comparing hashes (numbers) to the number of characters in the files
     # this is not an accurate representation of the size in memory, but is good enough
     print("---Before winnowing---")
-    print("File A has {0} hashes, {1:.2f}% of file".format(len(hashes_a),
-                                                           len(hashes_a)/len(source_a)*100))
-    print("File B has {0} hashes, {1:.2f}% of file".format(len(hashes_b),
-                                                           len(hashes_a)/len(source_a)*100))
+    print("File A has {0} hashes, {1} characters, {2:.2f}% of file".format(len(hashes_a), len(source_a),
+                                                                           len(hashes_a)/len(source_a)*100))
+    print("File B has {0} hashes, {1} characters, {2:.2f}% of file".format(len(hashes_b), len(source_b),
+                                                                           len(hashes_b)/len(source_b)*100))
     print("{0} of these are intersect ({1:.2f}%)".format(hash_intersection,
                                                          hash_intersection/min(len(hashes_a), len(hashes_b))*100))
     print("---After winnowing---")
@@ -27,7 +28,7 @@ def compare():
                                                            len(fingerprint_a)/len(source_a)*100))
     print("File B has {0} hashes, {1:.2f}% of file".format(len(fingerprint_b),
                                                            len(fingerprint_b)/len(source_b)*100))
-    print("{0} of these are intersect".format(len(set(fingerprint_a) & set(fingerprint_b))))
+    print("{0} of these are intersect".format(len(set(x[0] for x in fingerprint_a) & set(x[0] for x in fingerprint_b))))
 
 
 def get_ngrams(file_contents, n=10):
@@ -52,12 +53,16 @@ def get_ngrams(file_contents, n=10):
     return ngrams
 
 
-def get_hash_value(ngram_pairs):
-    hash_value = 0
-    for i in range(len(ngram_pairs)):
-        # TODO: use a rolling hash
-        hash_value += ord(ngram_pairs[i])*10**(len(ngram_pairs)-i-1)
-    return hash_value
+def get_hash_values(ngram_pairs):
+    hash_values = [None]*len(ngram_pairs)
+    for hash_index in range(len(ngram_pairs)):
+        _hash_value = 0
+        _current_ngram = ngram_pairs[hash_index][0]
+        for i in range(len(_current_ngram)):
+            # TODO: use a rolling hash
+            _hash_value += ord(_current_ngram[i])*10**(len(_current_ngram)-i-1)
+        hash_values[hash_index] = [_hash_value, ngram_pairs[hash_index][1]]
+    return hash_values
 
 
 def winnow(hashes, w=25):
@@ -65,7 +70,8 @@ def winnow(hashes, w=25):
     TODO: Reference this?
     We define variables in such a way that:
         1. If there is a substring match at least as long as the guaranteed threshold, t, then this match is detected
-        2. We do not detect any matches shorter than the noise threshold, n (used for our ngrams)
+        2. We do not detect any matches shorter than the noise threshold, n
+    Point (2) is already guaranteed because we use n to generate the ngrams
     We can now define the 'window size', w = t - k + 1
     This guarantees point (1) above
 
