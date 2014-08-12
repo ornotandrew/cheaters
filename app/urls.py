054ad11237cc5fc963c15055f0028be330ca252f
BLLBRE006 <brendan1b@hotmail.com>
2014-08-12 21:06:00 +0200

    removed csrf protection from  upload file POST

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url,patterns
+from django.views.decorators.csrf import csrf_exempt
 from app import views
 urlpatterns = patterns('',
 
-    url(r'^$',views.UploadFileView.as_view(),name='fileupload'),
+    url(r'^$',csrf_exempt(views.UploadFileView.as_view()),name='fileupload'),
     url(r'^report', views.ReportView.as_view(), name='report')
 
 )
