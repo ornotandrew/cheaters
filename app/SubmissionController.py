d2ab8911fe46a11d6e5eee5415f7cfa0f4e5fccf
BLLBRE006 <brendan1b@hotmail.com>
2014-08-22 19:55:07 +0200

    fixed upload file handler to properly extract files. added some debug prints in submission controller to check where most time is spent.

--- app/SubmissionController.py
+++ app/SubmissionController.py
@@ -1,3 +1,4 @@
+from app.models import Submission
 from lib.comparator import Comparator
 from lib.UploadFileHandler import FileHandler
 from lib.fingerprinter import Fingerprinter
@@ -13,7 +14,10 @@ class SubmissionController:
         for submission in submission_list:
             fingerprinter = Fingerprinter(submission.file_contents)
             submission.fingerprint = fingerprinter.fingerprint
-            submission.save()
 
+        print("done generating fingerprints")
+        Submission.objects.bulk_create(submission_list)
+        print("done saving submissions to db")
         comparator = Comparator(submission_list)
         self.report = comparator.report
+        print("done comparing")
--- lib/UploadFileHandler.py
+++ lib/UploadFileHandler.py
@@ -1,39 +1,41 @@
+import datetime
 import os
+import shutil
 import zipfile
 from zipfile import ZipFile, ZIP_DEFLATED
+import io
 from app.models import Submission
 from cheaters import settings
 
 class FileHandler():
 
     def __init__(self, file):
-        self.file = file
 
         self.submission_id = self.get_submission_id()
-        print(self.submission_id)
         self.submissions = []
         if zipfile.is_zipfile(file):
             self.process_zipfile(file)
+        print("done extracting")
 
     def process_zipfile(self, zfile):
 
-        with ZipFile(zfile, "r", compression=ZIP_DEFLATED) as zip:
-            directories = [item for item in zip.namelist() if item.endswith('/')]
-            print(directories)
-            print(zip.namelist())
+        with ZipFile(zfile) as zip:
 
             file_names = [file for file in zip.namelist() if not file.endswith("/")]
             for file_name in file_names:
+                    if not file_name.__contains__("__MACOSX"):
+                        submission = Submission()
+                        submission.submission_id = self.submission_id
+                        submission.user_id = os.path.dirname(file_name)
+                        if file_name.__contains__("MAC"):
+                            print(submission.user_id)
+                        submission.filename = os.path.basename(file_name)
+                        file = zip.open(file_name)
+                        submission.file_contents = file.read().decode('utf-8', "ignore")
+                        if len(submission.file_contents) != 0:
+                            self.submissions.append(submission)
 
-                    submission = Submission()
-                    submission.submission_id = self.submission_id
-                    submission.user_id = os.path.dirname(file_name)
-                    submission.file_name = os.path.basename(file_name)
-                    file = zip.open(file_name)
-                    submission.file_contents = file.read().decode('utf-8', "ignore")
 
-                    submission.save()
-                    self.submissions.append(submission)
 
     def get_submission_id(self):
         """
