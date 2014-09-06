f487a36a0ef4d908895983495f54b7916104cca7
BLLBRE006 <brendan1b@hotmail.com>
2014-09-06 21:25:30 +0200

    changed home page to report list and modified css padding for table columns

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url,patterns
 from django.views.decorators.csrf import csrf_exempt
 from app import views
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
+
 urlpatterns = patterns('',
 
-    url(r'^$',csrf_exempt(views.UploadFileView.as_view()),name="fileupload"),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy("report_list")),name="fileupload"),
     url(r'^report/(?P<report_id>[0-9]+)/(?P<file_1_id>[0-9]+)/(?P<file_2_id>[0-9]+)/$', views.ComparisonView.as_view(), name="report"),
     url(r'^report/(?P<report_id>[0-9]+)/$', views.ReportView.as_view(), name="report_file_list"),
     url(r'^report/list/$', views.ReportListView.as_view(), name="report_list")
