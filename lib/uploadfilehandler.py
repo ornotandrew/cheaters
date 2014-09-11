7ec46c4f36f056143e8061d75462bcb5ce3864e1
BLLBRE006 <brendan1b@hotmail.com>
2014-09-11 22:08:06 +0200

    changed uploadfilehandler to concatenate files with same userid

--- lib/uploadfilehandler.py
+++ lib/uploadfilehandler.py
@@ -18,26 +18,47 @@ class FileHandler():
             self.process_zipfile(file)
 
     def process_zipfile(self, zfile):
-
+        """
+        extracts the uploaded zipfile and saves the files to the database.
+        files for each user_id are concatenated into one file.
+        filters out binary files.
+        :param zfile:
+        :return: none
+        """
         with ZipFile(zfile) as zip:
+            submission = Submission()
+            file_paths = [file for file in zip.namelist() if not file.endswith("/")]
+            for file_path in file_paths:
+                    # check if file is text and not binary
+                    filetype = mimetypes.guess_type(file_path)[0]
+                    # filters out mac generated files which aren't detected by mimetypes
+                    if not file_path.__contains__("__MACOSX") and filetype == "text/plain":
 
-            file_names = [file for file in zip.namelist() if not file.endswith("/")]
-            for file_name in file_names:
-                    filetype = mimetypes.guess_type(file_name)[0]
-                    if not file_name.__contains__("__MACOSX") and filetype == "text/plain":
-                        print(file_name)
-                        submission = Submission()
-                        submission.submission_id = self.submission_id
-                        submission.user_id = os.path.dirname(file_name)
-                        if file_name.__contains__("MAC"):
+                        dirname = os.path.dirname(file_path)
+                        # gets the first directory of the dirname incase there are subfolders
+                        user_id = dirname.split("/")[0]
+                        # concatenate files if it is the same user as previous iteration
+                        # the file listing is in order of folders so this should always work to find all files for one user_id
+                        if submission.user_id == user_id:
+                            file = zip.open(file_path)
+                            file_content = file.read().decode('utf-8', "ignore")
+                            # if previous file was empty it wouldn't have been added to submission list so add it now
+                            if len(submission.file_contents) == 0 and len(file_content) != 0:
+                                self.submissions.append(submission)
+                            # append file to other files already in the submission
+                            submission.file_contents += file_content
+                        else:
+                            # if a different user_id make a new submission
+                            submission = Submission()
+                            submission.submission_id = self.submission_id
+                            submission.user_id = user_id
                             print(submission.user_id)
-                        submission.filename = os.path.basename(file_name)
-
-                        file = zip.open(file_name)
-
-                        submission.file_contents = file.read().decode('utf-8', "ignore")
-                        if len(submission.file_contents) != 0:
-                            self.submissions.append(submission)
+                            submission.filename = os.path.basename(file_path)
+                            file = zip.open(file_path)
+                            submission.file_contents = file.read().decode('utf-8', "ignore")
+                            # discard the submission if the file contents is zero lenth
+                            if len(submission.file_contents) != 0:
+                                self.submissions.append(submission)
 
 
 
