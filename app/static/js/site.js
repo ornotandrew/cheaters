f2896bb31a0d0c84f86719334954ad8ed4f103ed
Andrew van Rooyen <wraith.andrew@gmail.com>
2014-09-19 22:13:14 +0200

    Made the upload form sit EVEN NICER on the page

--- app/static/js/site.js
+++ app/static/js/site.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     $( "#id_file" ).on( "change", function updateFileName( event ){
        var input = $( this );
        var label = input.val().replace(/([^\\]*\\)*/,'');
-       $("#id_file_name").text(label);
+       $("#id_file_name").html("\</br\>\</br\>\</br\>\<b\>"+label+"\</b\>");
        });
 });
 
